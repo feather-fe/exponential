07bf0e62d29d3fdf7c6fd5fddc61e827abc6a0fc
Kai <kailunden9@gmail.com>
2024-12-18 02:26:06 +0000

    western union

--- api/makenewaccount.js
+++ api/makenewaccount.js
@@ -1,5 +1,5 @@
 import { json } from 'micro';
-export default async function handler() {
+export default async function handler(req, res) {
   const myHeaders = new Headers();
   myHeaders.append("Authorization", "Bearer patFPAzk3Ni4jtL7K.8bdcda86e17b32bd177f9ab25661e401e4454a8e4a2401a267c36b67e94ea933");
   myHeaders.append("Content-Type", "application/json");
