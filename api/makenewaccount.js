db7b3625a9ce60a03f3078d2061393ba4aac1d23
Kai <kailunden9@gmail.com>
2024-12-18 02:38:09 +0000

    barry bonds

--- api/makenewaccount.js
+++ api/makenewaccount.js
@@ -1,20 +1,5 @@
 import { json } from 'micro';
-export default async function handler(req, res) {
-  const myHeaders = new Headers();
-  myHeaders.append("Authorization", "Bearer patFPAzk3Ni4jtL7K.8bdcda86e17b32bd177f9ab25661e401e4454a8e4a2401a267c36b67e94ea933");
-  myHeaders.append("Content-Type", "application/json");
-  
-  const raw = JSON.stringify({
-    "fields": {
-      "username": "Very-Secure-Username.gov",
-      "password": "thosewhoknow"
-    }
-  });
-  const requestOptions = {
-    method: "GET",
-    headers: myHeaders,
-    redirect: "follow"
-  };
-  
-  fetch("https://api.airtable.com/v0/appXXbVu5p4uSKViT/logins", requestOptions)
+export function GET(request) {
+  return new Response(`Hello from ${process.env.VERCEL_REGION}`);
 }
+
