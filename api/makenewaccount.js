8ac340984f95fa99df9be38cb577a6a39738d59d
Kai <kailunden9@gmail.com>
2024-12-18 02:49:31 +0000

    still i think

--- api/makenewaccount.js
+++ api/makenewaccount.js
@@ -1,5 +1,7 @@
 import { json } from 'micro';
-export function POST(request) {
+export function POST(request
+  
+) {
 
 const myHeaders = new Headers();
 myHeaders.append("Authorization", "Bearer patFPAzk3Ni4jtL7K.8bdcda86e17b32bd177f9ab25661e401e4454a8e4a2401a267c36b67e94ea933");
