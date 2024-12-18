bcf55adac7aeee1a377eae2059219092f915fc41
Kai <kailunden9@gmail.com>
2024-12-18 02:05:23 +0000

    still

--- api/makenewaccount.js
+++ api/makenewaccount.js
@@ -3,7 +3,6 @@ export default async function handler() {
   const myHeaders = new Headers();
   myHeaders.append("Authorization", "Bearer patFPAzk3Ni4jtL7K.8bdcda86e17b32bd177f9ab25661e401e4454a8e4a2401a267c36b67e94ea933");
   myHeaders.append("Content-Type", "application/json");
-  myHeaders.append("Cookie", "brw=brwtcGK1QAbqebbws; brwConsent=opt-in; AWSALBTG=fQBXPbOQsQEHSXjDXvR+b6iFjtMkkEbhiT5k84Dc7zFYt7ll5DwQpDCIbcWVl2Ltle51IJ3/I5OW7PUlPufzkBMfJVERGRXO75xfJK6wj4zhN1tHSIE5mihHuoqDCsila3pFdAcNrEpDw7G+v8STeN0YEjvs4gMDA/YSgoSYss75u5wxGuM=; AWSALBTGCORS=fQBXPbOQsQEHSXjDXvR+b6iFjtMkkEbhiT5k84Dc7zFYt7ll5DwQpDCIbcWVl2Ltle51IJ3/I5OW7PUlPufzkBMfJVERGRXO75xfJK6wj4zhN1tHSIE5mihHuoqDCsila3pFdAcNrEpDw7G+v8STeN0YEjvs4gMDA/YSgoSYss75u5wxGuM=");
   
   const raw = JSON.stringify({
     "fields": {
