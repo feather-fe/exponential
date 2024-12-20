533fa8455c46c7a9b5270ca27e008224d15a127a
Kai <kailunden9@gmail.com>
2024-12-20 02:31:57 +0000

    new

--- netlify/functions/hello-world/hello-world.mjs
+++ netlify/functions/hello-world/hello-world.mjs
@@ -3,8 +3,8 @@ export default (request, context) => {
   try {
     const url = new URL(request.url)
     const subject = url.searchParams.get('name') || 'World'
-
-    return new Response(`Hello ${subject}`)
+    const { username, password } = request.json()
+    return new Response({username, password})
   } catch (error) {
     return new Response(error.toString(), {
       status: 500,
