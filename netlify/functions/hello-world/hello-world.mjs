6c349355d5992eb42614b78bb1f8b2e4fad3119f
Kai <kailunden9@gmail.com>
2024-12-20 02:35:05 +0000

    bakc again

--- netlify/functions/hello-world/hello-world.mjs
+++ netlify/functions/hello-world/hello-world.mjs
@@ -1,11 +1,16 @@
 // Docs on request and context https://docs.netlify.com/functions/build/#code-your-function-2
-export default (request, context) => {
+export default async (request, context) => {
+  if (request.method !== 'POST') {
+    return new Response('Method Not Allowed', {
+      status: 405,
+    })
+  }
+
   try {
-    const url = new URL(request.url)
-    const subject = url.searchParams.get('name') || 'World'
-    const { username, password } = request.json()
-    
-    return new Response({username, password})
+    const { username, password } = await request.json()
+    return new Response(JSON.stringify({ username, password }), {
+      headers: { 'Content-Type': 'application/json' },
+    })
   } catch (error) {
     return new Response(error.toString(), {
       status: 500,
