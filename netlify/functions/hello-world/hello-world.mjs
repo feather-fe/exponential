ab0eb524535e17eec136eed55949b139435ea153
Kai <kailunden9@gmail.com>
2024-12-23 20:08:19 +0000

    refactor account creation logic in hello-world function

--- netlify/functions/hello-world/hello-world.mjs
+++ netlify/functions/hello-world/hello-world.mjs
@@ -5,30 +5,31 @@ export default async (request, context) => {
       status: 405,
     })
   }
-  const myHeaders = new Headers();
-myHeaders.append("Content-Type", "application/json");
 
-const raw = JSON.stringify({
-  "username": username,
-  "password": password
-});
-
-const requestOptions = {
-  method: "POST",
-  headers: myHeaders,
-  body: raw,
-  redirect: "follow"
-};
-
-try {
-  const response = await fetch("https://exponential-psi.vercel.app/api/makenewaccount", requestOptions);
-  const result = await response.text();
-  console.log(result)
-} catch (error) {
-  return new Response("Error: Account not created", error);
-};
   try {
     const { username, password } = await request.json()
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    
+    const raw = JSON.stringify({
+      "username": username,
+      "password": password
+    });
+    
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow"
+    };
+    
+    try {
+      const response = await fetch("https://exponential-psi.vercel.app/api/makenewaccount", requestOptions);
+      const result = await response.text();
+      console.log(result)
+    } catch (error) {
+      return new Response("Error: Account not created", error);
+    };
     return new Response(JSON.stringify({ username, password }), {
       headers: { 'Content-Type': 'application/json' },
     })
